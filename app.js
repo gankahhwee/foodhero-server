f4b943e4b437710f164eac46d3bb74fdde59cd9f
Ubuntu <ubuntu@ip-172-31-43-103.us-west-2.compute.internal>
2016-06-27 08:52:46 +0000

    server

---
+++ app.js
@@ -0,0 +1,126 @@
+
+/**
+ * Module dependencies.
+ */
+//DATABASE CONNECT
+var express = require('express'),
+    app = express(),
+    routes = require('./routes'),
+    fs = require('fs'),
+    bodyParser = require('body-parser'),
+    expressJWT = require('express-jwt'),
+    jwt = require('jsonwebtoken');
+
+
+//DATABASE CONNECT
+//TODO: CREATE CUSTOM DATABASE USER FOR EXPRESS SERVER
+//TODO: Right now the passwords are stored in plaintext in mysql. Look up on how to encrypt or hash them
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'S9371014B',
+	database: 'ejabberd'
+});
+
+//var app = module.exports = express.createServer();
+//var server = require('https').createServer({
+//	key: fs.readFileSync('www.hayhaytheapp.com.key'),
+//	cert: fs.readFileSync('www.hayhaytheapp.com.chained.crt')
+//}, app);
+var server= require('http').createServer(app);
+
+//SHELL SPAWN
+const exec = require('child_process').exec;
+
+// Configuration
+
+//app.configure(function(){
+//  app.set('views', __dirname + '/views');
+//  app.set('view engine', 'jade');
+//  app.use(express.bodyParser());
+//  app.use(express.methodOverride());
+//  app.use(app.router);
+//  app.use(express.static(__dirname + '/public'));
+//});
+//
+//app.configure('development', function(){
+//  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+//});
+//
+//app.configure('production', function(){
+//  app.use(express.errorHandler());
+//});
+//
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(expressJWT({ secret: 'nyancat forever' }).unless({ path: ["/test"]}));
+// Routes
+
+app.get('/', routes.index);
+
+app.get('/test', function(req, res) {
+	//DATABASE TEST
+	connection.query('SELECT * FROM users', function(err,rows,fields) {
+		if (err) throw err;
+	});
+	
+	//COMMAND SHELL TEST
+	exec('ls', function(error, stdout, stderr) {
+		if(error) {
+			console.log('exec error: ${error}');
+			return;
+		}
+
+		console.log(stdout);
+		res.send("success");
+	});
+});
+
+app.post('/login', function(req, res) {
+	connection.query('SELECT * FROM users', function(err, rows, fields) {
+		var username = req.body.username;
+		var password = req.body.password;
+
+			
+	});
+});
+
+app.post('/get-events', function(req, res) {
+	connection.query('SELECT * FROM muc_room', function(err, rows, fields) {
+		console.log(rows);
+	});
+});
+
+app.post('get-room-number', function(req, res) {
+	var username = req.body.username;
+});
+
+app.post('/register', function(req, res) {
+	var username = req.body.username;
+	var email = req.body.email;
+	var password = req.body.password;
+	
+	//TODO: Check if this is susceptible to SQL Injection
+	connection.query('SELECT * FROM users WHERE username="'+username+'"', function(err,rows,fields) {
+	   console.log(rows);
+	   if(rows.length === 0){
+		exec('sudo ~/ejabberd-16.04/bin/ejabberdctl register ' + username + " foodhero.me " + password, function(error, stdout, stderr) {
+			if(error) {
+				console.log('exec error: ${error}');
+				return;
+			}
+			
+			res.send(stdout);
+	
+		});
+	   } else {
+		res.send('Sorry username already exists');
+	   }
+	});	
+});
+
+server.listen(8000);
