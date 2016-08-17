86e5b660f04e640a501b63030d621abe05724dcc
Lacie Fan Yuxin <lacie@velocloud.net>
2016-08-17 14:11:23 +0800

    POST going events

--- app.js
+++ app.js
@@ -257,6 +257,30 @@ app.post('/login', function(req, res) {
 	// });
 });
 
+app.post('/going-event', function(req, res) {
+	var username = req.body.username;
+	var event_id = req.body.event_id;
+	var going = req.body.event_id;
+
+	var query;
+
+	if(going) {
+		query = 'INSERT INTO food_events_attendants (username, event_id) VALUES ("'
+		+ username + '", '
+		+ event_id +' )';
+	} else {
+		query = 'DELETE FROM food_events_attendants WHERE username="' + username + '" AND event_id=' + event_id;
+	}
+	connection.query(query, function (err, rows, fields) {
+			if(err) {
+				console.log(err.stack);
+				return;
+			}
+
+			res.send({success: true});
+		});
+});
+
 app.post('/get-meals', function(req, res){
 	var username = req.body.username;
 	connection.query('SELECT COUNT(username) AS mealsShared FROM food_events WHERE username="' + username + '"', function(err2, rows2, fields2) {
